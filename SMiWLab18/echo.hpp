9d6b750752eec3f67fa444235f30e5967dc34003
filimie-stu <filimie885@student.polsl.pl>
2020-11-16 01:10:20 +0100

    Additional comment

--- SMiWLab18/echo.hpp
+++ SMiWLab18/echo.hpp
@@ -2,6 +2,11 @@
 
 #include <cstdio>
 
+//
+// For the function to work correctly, 
+// base file must be sampled at 44100
+// with signed 16-bit integer.
+//
 void apply_echo_to_file(
 	std::FILE & baseFile, 
 	int echoLayers, 
